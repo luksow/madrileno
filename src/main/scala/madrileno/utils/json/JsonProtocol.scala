1fa59df81021c624f4d4adf43a0b651783fff784
Łukasz Sowa <contact@luksow.com>
2025-02-08 22:00:05 +0100

    Dropping KebsCirce, exporting only required.

--- src/main/scala/madrileno/utils/json/JsonProtocol.scala
+++ src/main/scala/madrileno/utils/json/JsonProtocol.scala
@@ -1,18 +1,15 @@
 package madrileno.utils.json
 
 import org.http4s.circe.{CirceEntityDecoder, CirceEntityEncoder}
-import pl.iterators.kebs.circe.KebsCirce
 import pl.iterators.kebs.circe.enums.KebsCirceEnums
 import pl.iterators.kebs.instances.{NetInstances, TimeInstances, UtilInstances}
 
-trait JsonProtocol
-    extends CirceEntityDecoder
-    with CirceEntityEncoder
-    with KebsCirce
-    with KebsCirceEnums
-    with TimeInstances
-    with UtilInstances
-    with NetInstances {
+trait JsonProtocol extends CirceEntityDecoder with CirceEntityEncoder with KebsCirceEnums with TimeInstances with UtilInstances with NetInstances {
+  export pl.iterators.kebs.circe.flatDecoder
+  export pl.iterators.kebs.circe.flatEncoder
+  export pl.iterators.kebs.circe.instanceConverterEncoder
+  export pl.iterators.kebs.circe.instanceConverterDecoder
+
   type Encoder[A] = io.circe.Encoder[A]
   val Encoder: io.circe.Encoder.type = io.circe.Encoder
 
